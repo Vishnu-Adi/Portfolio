f8ac461f70d9c245fd0b82b35ff069f2d67b0e2c
Vishnu-Adi <111406816+Vishnu-Adi@users.noreply.github.com>
2025-01-06 19:17:14 +0530

    asd

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-  basePath: "/2048-in-react",
+  // output: "export",  // <=== enables static exports
+  // reactStrictMode: true,
+  // basePath: "/2048-in-react",
 };
 
 export default nextConfig;

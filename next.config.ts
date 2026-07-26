7cdf880a417bf1d10254e1b9712bd95bcdfacb91
Vishnu-Adi <111406816+Vishnu-Adi@users.noreply.github.com>
2026-07-26 12:35:03 +0530

    Prepare frontend for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: process.cwd(),
+  },
   // output: "export",  // <=== enables static exports
   // reactStrictMode: true,
   // basePath: "/2048-in-react",
